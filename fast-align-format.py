5ccac6befc7699458e22f61f3621eaed2ef87d7a
Florent-Marie <70381819+Florent-Marie@users.noreply.github.com>
2020-08-28 16:01:26 +0200

    Add files via upload

---
+++ fast-align-format.py
@@ -0,0 +1,107 @@
+# coding: utf-8
+"""
+	Programme qui fusionne deux textes dans un unique fichier texte 
+	après les avoir nettoyés afin que leur format soit accepté par 
+	fast_align.
+	
+	Usage : 
+		python fast-align-format.py texte1 texte2
+		
+		ou :
+		
+		python fast-align-format.py texte1 texte2 nomSortie
+	
+	Arguments:
+		
+		texte1 -- Chemin d'accès au fichier contenant le texte 
+		dans la langue source
+		
+		texte2 -- Chemin d'accès au fichier contenant le texte 
+		dans la langue cible
+		
+		nomSortie -- Chemin d'accès du fichier de sortie contenant la
+		fusion des deux textes. En cas d'absence de cet argument, un chemin
+		d'accès par défaut sera donné : Corpus_fast-align.txt. 
+"""
+import sys
+import re
+import os
+
+def tab(text):
+	table=[]
+	for line in text:
+		line=line.strip()
+		table.append(line)
+	return table
+
+def propre(s):
+	regex= re.compile(r"\s(\W|·)\s")
+	new=regex.sub(" ",s)
+	new=new.replace("ʼ","'")
+	new=new.replace("ʼ ","'")
+	new=new.replace("' ","'")
+	new=new.replace("’ ","'")
+	new=new.replace("’","'")
+	
+	new=new.replace("“","\"")
+	new=new.replace("”","\"")
+	regex= re.compile(r"^\W+",re.M)
+	new=regex.sub("",new)
+	return new
+
+
+#On récupère le nom des fichiers à ouvrir
+
+srcFile=sys.argv[1]
+trgtFile=sys.argv[2]
+print (sys.argv)
+if (len(sys.argv)==4):
+	sortie=sys.argv[3]
+else:
+	sortie="Corpus_fast-align.txt"
+
+#On ouvre les fichiers
+
+src = open(srcFile, "r", encoding="utf8")
+trgt = open(trgtFile, "r", encoding="utf8")
+
+#On lit les fichiers et on récupère les contenus dans la variable text
+srcTab=tab(src)
+trgtTab=tab(trgt)
+corpus=""
+regex= re.compile(r"«\W|\W»|[:?!]")
+regex2=re.compile(r" · ")
+	
+for i in range(0,len(srcTab)):
+	print ("Total number of lines : "+str(len(srcTab))+". "+str(len(srcTab)-i)+ " lines remaining.")
+	srcPhrase=propre(srcTab[i])
+	trgtPhrase=propre(trgtTab[i])
+	if (srcPhrase!="" and trgtPhrase!=""):
+		ligne=srcPhrase+" ||| "+trgtPhrase+"\n"
+		ligne=regex.sub("",ligne)
+		ligne=ligne.replace("  "," ")
+		ligne=regex2.sub(", ",ligne)
+		ligne=re.sub(r"(\w)(\.)(\w)",r"\g<1>. \g<3>",ligne)
+		ligne=re.sub(r"(\s)(,|\.)(\s)",r"\g<2> \g<3>",ligne)
+		ligne=ligne.replace("  "," ")
+		corpus+=ligne
+
+
+#On affiche (on enregistre) les résultats
+
+fichier = open(sortie, "w", encoding="utf8")
+fichier.write(corpus)
+fichier.close()
+
+#On ferme les fichiers
+trgt.close()
+src.close()
+
+print("Ecriture terminée")
+# ~ print("Ouverture du fichier dans fast_align.")
+# ~ os.system("cd ../fast_align-master/build/")
+# ~ truc=os.popen("./fast_align -i Corpus_fast-align.txt -d -o -v")
+# ~ ah=truc.read()
+# ~ print(ah)
+# ~ print("Terminé")
+
---
+++ traitement_aligner.py
@@ -0,0 +1,277 @@
+# coding: utf-8
+"""
+	Programme qui produit l'extraction lexicale selon l'alignement de chaque 
+	mot et qui enregistre chaque mot du texte et tous ses alignements 
+	dans deux dictionnaires, l'un de la langue source à la langue cible, 
+	l'autre dans le sens inverse.
+	Les deux dictionnaires sont ensuite consultables par l'utilisateur.
+	
+	Usage : 
+		python traitement_aligner.py nomFichierTexte nomFichierAlignement
+	
+	Arguments:
+		
+		nomFichierTexte -- Chemin d'accès au fichier contenant le texte 
+		bilingue utilisé par l'aligneur
+		
+		nomFichierTexte -- Chemin d'accès au fichier contenant l'alignement 
+		réalisé par Fast-align
+"""
+import sys
+import re
+
+########################################################################
+###########################	ZONE DE FONCTIONS	########################
+########################################################################
+def best(h, clé):
+	"""
+	Fonction récupérant le mot le plus utilisé pour traduire le mot clé 
+	recherché en inversant les couples mot/nombre d'occurences parmi les 
+	valeurs du dictionnaire
+	"""
+	invMax={}
+	for mot in h[clé]:
+		invMax[h[clé][mot]]=mot
+	return(invMax[max(invMax.keys())])
+	
+def tab(fichier):
+	"""
+	Fonction qui permet après lecture d'un fichier d'ajouter chacune des 
+	lignes de ce fichier comme élément d'une liste.
+	"""
+	table=[]
+	for line in fichier:
+		line=line.strip()
+		table.append(line)
+	return table
+
+def propre(s):
+	"""
+	Fonction qui épure le texte de caractères de ponctuations non-conformes,
+	comme les apostrophes différentes et les signes de ponctuation gênants
+	qui seraient encore présents dans le texte.
+	"""
+	regex= re.compile(r"\s(\W|·)\s")
+	new=regex.sub(" ",s)
+	new=new.replace("ʼ","'")
+	new=new.replace("ʼ ","'")
+	new=new.replace("' ","'")
+	new=new.replace("’ ","'")
+	new=new.replace("’","'")
+	new=new.replace("“","\"")
+	new=new.replace("”","\"")
+	new=new.replace("( ","(")
+	new=new.replace(" )",")")
+	new=new.replace("'","' ")
+	new=new.replace("' )","')")
+	return new
+
+def coordonnees(phrase):
+	"""
+	Fonction qui repère les coordonées type latitudes/longitudes et 
+	remplace l'espace qui les sépare par [SPACE]
+	"""
+	phrase=re.sub(r"(\(?(-?\d+\d*([$€£%'°]))+)(\s)((\d+\d*([$€£%'°]))+\))",r"\g<1>[SPACE]\g<5>",phrase)
+	return phrase
+
+def tri(dictionnaire):
+	"""
+	Cette fonction (à compléter) permettra de faire un tri dans les cas où
+	on a beaucoup de correspondances de traductions pour un mot.
+	"""
+	dico=dictionnaire
+	# ~ if len(dico)>3:
+		
+		# ~ for mot in dico :
+			
+			# ~ print("quelque chose")
+	return (dico)
+
+########################################################################
+###########################	ZONE DE CODE	############################
+########################################################################
+
+#On récupère le nom des fichiers à ouvrir
+
+srcFile=sys.argv[0]
+alignement=sys.argv[1]
+
+#On ouvre les fichiers
+
+src = open(srcFile, "r", encoding="utf8")
+al=open(alignement, "r", encoding="utf8")
+
+#On lit le fichier du corpus bilingue et on récupère les phrases.
+#On les place respectivement dans deux tableaux : 
+#"srctab" pour la langue source et "tgttab" pour la langue cible
+
+lineTab=tab(src)
+srctab=[]
+tgttab=[]
+
+for i in range(0,len(lineTab)):
+	temptab=[]
+	temptab=lineTab[i].split(" ||| ")
+	srctab.append(propre(temptab[0]))
+	tgttab.append(propre(temptab[1]))
+
+#On récupère le contenu du fichier fast-align dans le tableau "lineAl"
+
+lineAl=tab(al)
+
+#On ferme les fichiers
+
+src.close()
+al.close()
+
+#On crée deux hashages qui contiendront les mots recherchés (clés) 
+#et leurs traductions possibles (valeurs)
+#"fr" pour la langue source, le français, et "en" pour la cible, l'anglais. 
+#(Mais cela fonctionnera peu importe le couple de langue)
+fr={}
+en={}
+
+#On vérifie si la taille des deux fichiers (corpus et alignements) est identique.
+#Sinon on affiche un message d'erreur.
+
+if (len(lineTab)==len(lineAl)):
+	
+#Ensuite on fait défiler les éléments des tableaux pour récupérer les 
+#alignements de chaque mot de la phrase en cours.
+
+	for i in range(0,len(lineAl)):
+		
+#Dans "corr" (pour "correspondance") on a toutes les paires de chiffres
+#séparés par des "-", le premier (corr[n].split("-")[0]) correspond à l'id
+#d'un mot français dans "srctab", le second à celui d'un mot anglais dans "tgttab"
+
+		corr=lineAl[i].split(" ")
+
+#Ici on règle les problèmes liés à des coordonées gps dans un texte 
+#(il faut compter les doordonnées comme un seul mot)
+#On remplace les espaces des coordonées par [SPACE] puis on découpe la phrase
+#Aux espaces restants pour ranger chaque mot dans les tableaux "frMots" et "enMots"
+
+		frMots=coordonnees(srctab[i]).split(" ")
+		enMots=coordonnees(tgttab[i]).split(" ")
+
+#"n" représente l'indice de chaque paire de correspondance dans "corr"
+
+		for n in range (0, len(corr)):
+			key=int(corr[n].split("-")[0])
+			val=int(corr[n].split("-")[1])
+#"key" et "val" sont des indices correspondants aux mots présents respctivement dans frMots et enMots
+
+			if (key==len(frMots) or val==len(enMots)):
+				break
+#Ce if permet de terminer la ligne si jamais le tableau frMots est plus court que que veut trouver l'indice key
+
+#On enleve les potentielles ponctuactions collées aux mots
+
+#Pour cela on écrit d'abord une expression régulière qui va rechercher les mots qu'on dira bien formés
+			noPonctuation= re.compile(r"(\(.[^)]*\)|\d+(,|\.|\s)?\d*(\s*[$€£])*|€|\w+)",re.U)
+
+#On remplace les [SPACE] par des espaces normaux
+			for m in range (0,len(frMots)):
+				frMots[m]=frMots[m].replace("[SPACE]"," ")
+			for m in range (0,len(enMots)):
+				enMots[m]=enMots[m].replace("[SPACE]"," ")
+				
+#Si le mot n'est pas "bien formé", on estime qu'il s'agit d'une ponctuation.
+#Dans ce cas on le remplace par "[punct]"
+			if noPonctuation.search(frMots[key]):
+				fran=noPonctuation.search(frMots[key])
+				fran=fran.group().casefold()
+			else:
+				fran="[punct]"
+			if noPonctuation.search(enMots[val]):
+				angl=noPonctuation.search(enMots[val])
+				angl=angl.group().casefold()
+			else:
+				angl="[punct]"
+				
+#On ajoute le mot actuel au dictionnaire
+			if (fran in fr.keys()): 
+				if (angl not in fr[fran].keys()):
+					fr[fran][angl]=1
+				else :
+					fr[fran][angl]=fr[fran][angl]+1
+			else:
+				fr[fran]={angl:1}
+#Dans l'autre sens pour la langue cible
+				
+			if (angl in en.keys()): 
+				if (fran not in en[angl].keys()):
+					en[angl][fran]=1
+				else :
+					en[angl][fran]=en[angl][fran]+1
+			else:
+				en[angl]={fran:1}
+#On calcule un pourcentage de ce qui a été réalisé et on l'affiche
+		pourcentage=((i+1)*100)/(len(lineAl))
+		
+		if (pourcentage!=100):
+			print ("Avancement : "+str(pourcentage)+"%.")
+	print ("Terminé !")
+	
+else:
+	print("Les deux fichiers n'ont pas le même nombre de lignes. Vérifiez que vous avez sélectionné les bons fichiers.")
+
+#On peut enregistrer les dictionnaires dans un fichier
+
+# ~ fichier = open("results.txt", "w", encoding="utf8")
+# ~ fichier.write(fr)
+# ~ fichier.close()
+
+########################################################################
+###############	ZONE DE TESTS DES RESULTATS DE FAST ALIGN	############
+########################################################################
+
+"""
+Cette partie est reservée à la phase de tests sur l'alignement produit 
+par Fast_Align.
+Ici, on va essayer de récupérer un fichier txt contenant plusieurs 
+paragraphe.
+
+Chaque paragraphe est constitué de 5 lignes :
+
+Ligne 1 : Le numéro de ligne.
+Ligne 2 : La phrase en français.
+Ligne 3 : La phrase en anglais.
+Ligne 4 : La ligne correspondant dans le fichier fast align.
+Ligne 5 : l'alignement mot à mot transcrit par ce script.
+"""
+
+########################################################################
+###########################	ZONE INTERFACE UTILISATEUR	################
+########################################################################
+
+fini=False
+while not (fini==True):
+	print("Pour afficher le dictionnaire français-anglais : fr\nPour afficher le dictionnaire anglais-français : en\nPour effectuer une recherche par mot clé : r\nPour effectuer une recherche par mot clé de la traduction la plus utilisée pour un mot : r+\nPour quitter : q")
+	action=str(input("Votre choix : ")).casefold()
+	if (action=="r"):
+		mot=str(input("Entrez le mot à chercher : ")).casefold()
+		if mot in fr.keys():
+			print (tri(fr[mot]))
+		elif mot in en.keys():
+			print (tri(en[mot]))
+		else :
+			print("Le mot demandé n'est pas répertorié.")
+	elif (action=="r+"):
+		mot=str(input("Entrez le mot à chercher : ")).casefold()
+		if mot in fr.keys():
+			print ("Traduction de \""+mot+"\" la plus utilisée : "+best(fr,mot))
+		elif mot in en.keys():
+			print ("Traduction de \""+mot+"\" la plus utilisée : "+best(en,mot))
+		else :
+			print("Le mot demandé n'est pas répertorié.")
+	elif (action=="fr"):
+		print(fr)
+	elif (action=="en"):
+		print(en)
+	else:
+		print ("L'action saisie n'est pas reconnue.")
+	continuer=str(input("Appuyez sur Entrée pour continuer, ou écrivez Q puis appuyez sur Entrée pour arrêter : ")).casefold()
+	if (continuer=="q"):
+		fini=True
